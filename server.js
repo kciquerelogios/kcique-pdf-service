4c10df2e497c4d7bae119b846791e74cc2e4e94c
kciquerelogios <kciqueadm@gmail.com>
2026-06-08 02:26:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const fs = require('fs');
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 const SECRET = process.env.PDF_SECRET || 'kcique2026';
